58968cf5c56615f7caf761705e77d0171ed9e9cc
Stuart Laverick <appsol@gmail.com>
2017-08-24 10:06:21 +0100

    Added DOCUMENTROOT variable to move server DocumentRoot

--- php7.0/setup.sh
+++ php7.0/setup.sh
@@ -20,10 +20,15 @@ if [[ ! -z "$LARAVEL" ]]; then
     composer global require "laravel/installer"
     echo >&2 "Laravel: creating new $LARAVEL"
     laravel new $LARAVEL
+    echo >&2 "Set DocumentRoot for new Laravel install"
     sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$LARAVEL\/public/" /etc/apache2/sites-available/000-default.conf
     sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$LARAVEL\/public/" /etc/apache2/sites-available/default-ssl.conf
+elif [[ ! -z "$DOCUMENTROOT" ]]; then
+    echo >&2 "Set DocumentRoot as /var/www/html/$DOCUMENTROOT"
+    sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$DOCUMENTROOT/" /etc/apache2/sites-available/000-default.conf
+    sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$DOCUMENTROOT/" /etc/apache2/sites-available/default-ssl.conf
 else
-    echo >&2 "Composer: skipping Laravel install"
+    echo >&2 "DocumentRoot unchanged"
 fi
 
 if [[ -f "composer.json" ]]; then
